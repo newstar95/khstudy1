e84764fb7e09550a798099843ce03c0c9873db78
newstar95 <byeol12120@gmail.com>
2023-06-20 15:56:17 +0900

    실습 문제 풀이2

---
+++ day02/src/data/Test09.java
@@ -0,0 +1,24 @@
+package data;
+
+public class Test09 {
+	public static void main(String[] args) {
+		//내 풀이
+		long money = 2000000L;
+		int month = 12;
+		int year = 30;
+		
+		long total = (money * month) * year;
+		
+		
+		//강사님 풀이
+		//int money = 2000000;
+		//int year = 30;
+				
+		//int month = year * 12;
+		//int total = money * month;
+		
+		System.out.println(total);
+		
+		
+	}
+}
---
+++ day02/src/data/Test10.java
@@ -0,0 +1,45 @@
+package data;
+
+public class Test10 {
+	public static void main(String[] args) {
+		//내 풀이
+//		int time = 60;
+//		int minute = 1;
+//		
+//		int yesterdayTime = 2;
+//		int yesterdayMinute = 45;
+//		
+//		int todayTime = 1;
+//		int todayMinute = 50;
+//		
+//		int yesterday = (time * yesterdayTime) + (minute * yesterdayMinute);   
+//		int today = (time * todayTime) + (minute * todayMinute); 
+//		
+//		int total = yesterday + today;
+//		int hour = total / 60;
+//		int hour = total % 60;
+		
+//		System.out.println(total);
+		
+		
+		//강사님 풀이
+		//시간은 가장 작은 단위로 변환하여 푼다.
+		//입력
+		int yesterdayHour = 2, yesterdayMin = 45;
+		int todayHour = 1, todayMin = 50; 
+		
+		//계산
+		int yesterdayTime = yesterdayHour * 60 + yesterdayMin;
+		int todayTime =  todayHour * 60 + todayMin;
+		
+		int time = yesterdayTime + todayTime;
+		int hour = time / 60;
+		int minute = time % 60;
+		
+		//출력 
+		System.out.println(hour);
+		System.out.println(minute);
+		
+		
+	}
+}

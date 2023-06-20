a79d5ff260c62305ca604e80f2caf84c2a551ee5
newstar95 <byeol12120@gmail.com>
2023-06-20 11:00:01 +0900

    과정명 수정

--- day02/src/day02/Information.java
+++ day02/src/day02/Information.java
@@ -2,7 +2,7 @@ package day02;
 
 public class Information {
 	public static void main(String[] arsg) {
-		System.out.println("과정명: 프로그래밍 언어 활용");
+		System.out.println("과정명: 공공데이터 융합 웹 애플리케이션 개발자 양성과정4");
 		System.out.println("강사명: 황인빈 강사님");
 		System.out.println("수강생 이름: 장새별");
 	}

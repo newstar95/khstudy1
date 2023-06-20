054329333053fbb3945c35fd167f93063e2bf3f5
newstar95 <byeol12120@gmail.com>
2023-06-20 10:54:01 +0900

    Information 파일 생성, 출력 명령 활용

---
+++ day02/src/day02/Information.java
@@ -0,0 +1,9 @@
+package day02;
+
+public class Information {
+	public static void main(String[] arsg) {
+		System.out.println("과정명: 프로그래밍 언어 활용");
+		System.out.println("강사명: 황인빈 강사님");
+		System.out.println("수강생 이름: 장새별");
+	}
+}

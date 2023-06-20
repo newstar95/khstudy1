880a091cfc94b8c1a7a841182efd132de487b9bc
newstar95 <byeol12120@gmail.com>
2023-06-20 13:15:15 +0900

    출력 명령1

--- day02/src/data/Test01.java
+++ day02/src/data/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		//따옴표를 쓰는 기준은 "계산"을 하는가이다.
 		System.out.println("2023-06-20"); //글자 출력 
-		System.out.println(2023-06-20);	// 숫자 출력(계산식)
+		System.out.println(2023-06-20);	// 숫자 출력(계산식) 
 		
 		
 	}

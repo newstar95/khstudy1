8fcdc20618d879bc5362b31156bdcd2b3f535bd9
newstar95 <byeol12120@gmail.com>
2023-06-21 14:21:15 +0900

    성인 판별 프로그램 예제

---
+++ day03/src/data3/Test02.java
@@ -0,0 +1,16 @@
+package data3;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+			
+		int birth = 2003;
+		int year = 2023;
+		
+//		int age = year - birth + 1;
+		boolean choice = (year - birth + 1 ) >= 20;
+		
+		System.out.println(choice);
+		
+	}
+}

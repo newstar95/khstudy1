48415b6dfbc44719291a9ca27c89b4d3ac90b1cd
newstar95 <byeol12120@gmail.com>
2023-06-21 13:38:20 +0900

    실수와 정수를 섞어서 한 예제 풀이

---
+++ day03/src/data2/Test03.java
@@ -0,0 +1,14 @@
+package data2;
+
+public class Test03 {
+	public static void main(String[] args) {
+		int price = 1530;
+		int money = 50000;
+		
+		double liter = money / (double)price;	
+//		double liter = money / price;
+		
+		System.out.println(liter);
+		
+	}
+}

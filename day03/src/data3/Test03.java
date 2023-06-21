5b0904e6cbbb25162697e999c6dd5cefc533edf2
newstar95 <byeol12120@gmail.com>
2023-06-21 14:57:32 +0900

    짝수인지 판단하는 논리형 데이터 예제

---
+++ day03/src/data3/Test03.java
@@ -0,0 +1,15 @@
+package data3;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		int num = 17;
+		
+		boolean even = (num % 2) == 0; //보통 이렇게 씀
+		// int mod = num % 2;
+		// boolean zero = mod == 0;
+		
+		System.out.println(even);
+		
+	}
+}

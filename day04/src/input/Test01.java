836de9287aac4229c0e3f967a7ef1555e1d1ad34
newstar95 <byeol12120@gmail.com>
2023-06-22 13:03:54 +0900

    System.in 을 안 쓰는 이유

---
+++ day04/src/input/Test01.java
@@ -0,0 +1,13 @@
+package input;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//표준 입력 
+		//- System.in 을 사용하여 처리
+		//- 사용자가 입력해주기를 기다려야 한다.
+		//-ASCII 코드를 입력받는다. --> System.in.read();
+		 
+		
+	}
+}

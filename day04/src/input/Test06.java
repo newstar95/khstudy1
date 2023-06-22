8606e3b41106611a4b4eee705daefd3368ba44ab
newstar95 <byeol12120@gmail.com>
2023-06-22 15:44:39 +0900

    문자열 예제

---
+++ day04/src/input/Test06.java
@@ -0,0 +1,20 @@
+package input;
+
+import java.util.Scanner;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		//문자열 입력
+		//.next() 명령으로 띄어쓰기 전까지 입력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("이름이 무엇입니까: ");
+		String name = sc.next();
+		System.out.println("입력한 이름 = " + name);
+		
+		sc.close();
+
+	}
+}

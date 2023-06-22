e7202a5d770475663caa4ef9c7cfa9bcd255cb29
newstar95 <byeol12120@gmail.com>
2023-06-22 16:10:11 +0900

    조건문의 시작 / 홀수, 짝수

---
+++ day04/src/condition/Test01.java
@@ -0,0 +1,19 @@
+package condition;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//조건(Condition)
+		
+		int number = 5;
+		
+		if( number % 2 != 0 ) {
+		System.out.println("홀수");
+		}
+		
+//		if ( number % 2 == 0 ) {
+		else {
+		System.out.println("짝수");
+		}
+	}
+}

af022457684847e30a4e69a79a5edb368f9e568d
newstar95 <byeol12120@gmail.com>
2023-06-22 16:21:40 +0900

    스캐너 + if문 실습 / 출생년도 이벤트

--- day04/src/condition/Test02.java
+++ day04/src/condition/Test02.java
@@ -6,21 +6,23 @@ public class Test02 {
 
 	public static void main(String[] args) {
 		//입력
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("출생년도(YYYY)를 입력하세요. : ");
-		int birth = sc.nextInt();
-		
-		sc.close();
-		
-		//계산
-		int year = 2023;
-		int age = year - birth + 1;
-		
-		if (age == 19) {
-			System.out.println(age + "살 이므로 이벤트 대상입니다.");
-		} else {
-			System.out.println(age + "살 이므로 이벤트 대상이 아닙니다.");
-		}
+				Scanner sc = new Scanner(System.in);
+				
+				System.out.print("출생년도(YYYY)를 입력하세요. : ");
+				int birth = sc.nextInt();
+				
+				sc.close();
+				
+				//계산
+				int year = 2023;
+				int age = year - birth + 1;
+				
+				if (age == 19) {
+					System.out.println(age + "살 이므로 이벤트 대상입니다.");
+				} else {
+					System.out.println(age + "살 이므로 이벤트 대상이 아닙니다.");
+				}
+				
+			}
 	}
-}
+

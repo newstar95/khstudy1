d29493f97ff1ab2472d7698430bca694563e93c7
newstar95 <byeol12120@gmail.com>
2023-06-23 17:27:13 +0900

    for문 예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// 반복(loop)
+		
+		for(int i = 0; i < 10; i++ ) // (i++) or (i = i + 1) or (i += 1)
+			System.out.println("Hello world!");
+	}
+}

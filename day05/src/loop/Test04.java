a562ef5d0a3815a5b9d57b554393f70a3251e401
newstar95 <byeol12120@gmail.com>
2023-06-26 09:44:56 +0900

    5가 들어간 숫자 찾기

--- day05/src/loop/Test04.java
+++ day05/src/loop/Test04.java
@@ -3,10 +3,10 @@ package loop;
 public class Test04 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 100; i++) {
-			if ( i == 3 || i == 6 || i == 9) {
+		for( int i = 1; i < 100; i++ ) {
+			if(i / 10 == 5 || i % 10 == 5) { //10의 자리가 5라고 표현할 수도 있지만 50 < 60 이렇게 표현할 수도 있다.
 				System.out.println(i);
 			}
-		}
-	}
+		}	
+	}	
 }

c21cd1c943d8479869ade5cffe8814d5a91e90b3
newstar95 <byeol12120@gmail.com>
2023-06-26 09:55:17 +0900

    369 숫자 찾기 문제 풀이

--- day05/src/loop/Test05.java
+++ day05/src/loop/Test05.java
@@ -3,8 +3,16 @@ package loop;
 public class Test05 {
 
 	public static void main(String[] args) {
-		
-		
+		for (int i = 1; i < 100; i++) {
+			int one = i % 10;
+			int ten = 1 / 10;
+			
+			boolean numOne= one == 3 || one == 6 || one == 9; 
+			boolean numTen= ten == 3 || ten == 6 || ten == 9;
+			
+			if ( numOne || numTen ) {
+				System.out.println(i);
+				}
+			}
+		}
 	}
-
-}

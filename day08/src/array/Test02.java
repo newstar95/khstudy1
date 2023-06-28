df4d2b90f1e09d7789e29f84641abd29a8d1102e
newstar95 <byeol12120@gmail.com>
2023-06-28 10:00:13 +0900

    배열의 사용 예제

--- day08/src/array/Test02.java
+++ day08/src/array/Test02.java
@@ -14,12 +14,19 @@ public class Test02 {
 		//휴대폰이랑 휴대폰 진열대의 차이이다.
 		//차로 치면 주차장같은 존재
 		
+//		a = 10; //error(컴파일에러)
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		a[3] = 40; //코드 상의 오류는 없으나 실행하면 error(런타임에러)
+		
 		System.out.println(a); //리모컨 정보 출력, 쓸 일이 없음
-		System.out.println(a[0]);
-		System.out.println(a[1]);
-		System.out.println(a[2]);
+		System.out.println(a[0]); //a가 보는 +0지점 데이터
+		System.out.println(a[1]); //a가 보는 +1지점 데이터
+		System.out.println(a[2]); //a가 보는 +2지점 데이터
+		System.out.println(a[3]); //없음(error)
 		
-		
+	
 		
 		
 

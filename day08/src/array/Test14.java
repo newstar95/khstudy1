b9093e19e20baf437f6b41a3c438b63f6a04c9e3
newstar95 <byeol12120@gmail.com>
2023-06-28 18:11:43 +0900

    과제 풀이

--- day08/src/array/Test14.java
+++ day08/src/array/Test14.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Random;
+
 public class Test14 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,19 @@ public class Test14 {
 		//1. 모든 자리는 1번씩 다른 위치와 바뀌어야 한다.
 		//2. 어디랑 섞일지 대상 위치는 모르며 같은 위치일 수도 있다. (1번에 있는데 1번이 나올 수 있다)
 		//3. 랜덤으로 정해진 위치와 현재 위치의 데이터를 바꾸도록 구현
-
+		int[] data = new int[] {30, 50, 20, 10, 40};
+		
+		Random r = new Random();
+		int ranIndex = r.nextInt(5);
+		
+		for (int i = 0; i < data.length; i++) {
+			int temp = data[i];
+			data[i] = data[ranIndex];
+			data[ranIndex] = temp;
+		}
+		
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+		}
 	}
 }

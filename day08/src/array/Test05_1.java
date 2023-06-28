c0b6dca0707167ffb7bd17422438f31b9a3323e1
newstar95 <byeol12120@gmail.com>
2023-06-28 17:43:42 +0900

    반복문과 배열 사용 예제

--- day08/src/array/Test05_1.java
+++ day08/src/array/Test05_1.java
@@ -2,25 +2,27 @@ package array;
 
 import java.util.Scanner;
 
-public class Test5_2 {
+public class Test05_1 {
 
 	public static void main(String[] args) {
 		//배열과 반복문
 		//-ex : 3개의 숫자를 입력받아서 출력, 합계 구하기
+		//- 배열에 크기가 정해지면 자동으로 length라는 버튼이 생긴다.
 		
 		int[] data = new int[3];
-		//data ----> [0][0][0]
+		//data ----> [0][0][0] (length=3)
+		System.out.println(data.length);
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("숫자 3개 입력");
-		for(int i = 0; i <= 2; i++) {
+		System.out.println("숫자 입력");
+		for(int i = 0; i < data.length; i++) {
 		data[i] = sc.nextInt();
 		}
 		
 		sc.close();
 		
-		for(int i = 0; i <= 2; i++) {
+		for(int i = 0; i < data.length; i++) {
 			System.out.println(data[i]);
 		}
 	}
--- day08/src/array/Test05_2.java
+++ day08/src/array/Test05_2.java
@@ -2,27 +2,25 @@ package array;
 
 import java.util.Scanner;
 
-public class Test5_1 {
+public class Test05_2 {
 
 	public static void main(String[] args) {
 		//배열과 반복문
 		//-ex : 3개의 숫자를 입력받아서 출력, 합계 구하기
-		//- 배열에 크기가 정해지면 자동으로 length라는 버튼이 생긴다.
 		
 		int[] data = new int[3];
-		//data ----> [0][0][0] (length=3)
-		System.out.println(data.length);
+		//data ----> [0][0][0]
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("숫자 입력");
-		for(int i = 0; i < data.length; i++) {
+		System.out.println("숫자 3개 입력");
+		for(int i = 0; i <= 2; i++) {
 		data[i] = sc.nextInt();
 		}
 		
 		sc.close();
 		
-		for(int i = 0; i < data.length; i++) {
+		for(int i = 0; i <= 2; i++) {
 			System.out.println(data[i]);
 		}
 	}

284c4ffd823917237852ce182d75108cac1f36ca
newstar95 <byeol12120@gmail.com>
2023-06-28 09:46:01 +0900

    배열이 왜 필요할까?

---
+++ day08/src/array/Test01.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		//배열(array)
+		//배열이 필요한 상황
+		//ex: 숫자 3개를 입력받아서 출력하는 프로그램
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자 3개를 입력하세요");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		
+		
+		
+		
+
+	}
+}

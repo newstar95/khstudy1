050ea6f75444f568296b631b289667dc2f035712
newstar95 <byeol12120@gmail.com>
2023-06-29 14:34:07 +0900

    버블 정렬

--- day09/src/array2/Test02_3.java
+++ day09/src/array2/Test02_3.java
@@ -11,6 +11,11 @@ public class Test02_3 {
 		
 		//버블 정렬 k회차(인접한 두 개의 항목을 비교하여 큰 값을 우측으로 이동)
 		//[0] vs [1] / [1] vs [2] / [2] vs [3] / [3] vs [4]
+//		
+//		<기초 정렬>
+//		선택 정렬(Selection sort)	최소값 찾아서 교체하는 방식
+//		버블 정렬(Bubble sort)	인접한 두 항목을 비교하여 교체하는 방식
+//		삽입 정렬(Insertion sort)	
 
 		for(int k=data.length-2; k >=0; k--) {
 			for(int i = 0; i <= k; i++) {

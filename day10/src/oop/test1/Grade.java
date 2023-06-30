48992ca269e36016b13b17dc051e9c480ede8cd3
newstar95 <byeol12120@gmail.com>
2023-06-30 10:45:51 +0900

    클래스 및 객체 생성 예제

---
+++ day10/src/oop/test1/Grade.java
@@ -0,0 +1,14 @@
+package oop.test1;
+
+//시험 성적 정보를 자바에게 설명하기 위해 만드는 도구(클래스)
+public class Grade {
+	//시험 성적 정보의 구성요소 데이터를 정의(멤버 필드)
+	String name;
+	int korean;
+	int english;
+	int math;
+	//변수 네 개를 묶어서 grade라고 부르겠다. 라고 자바에게 알려주는 것
+	//지금은 객체가 생긴 게 아니고 클래스가 생긴 것, 클래스는 객체를 만들기 위한 설계도 같은 것.
+
+}
+

99d9c1337d0f90b6b035aef94db2512fca966270
newstar95 <byeol12120@gmail.com>
2023-07-03 17:05:41 +0900

    캐릭터 정보 문제

--- day11/src/oop/constructor3/Test01.java
+++ day11/src/oop/constructor3/Test01.java
@@ -6,12 +6,10 @@ public class Test01 {
 		Character a = new Character("헤라클레스", "전사", 50);
 		Character b = new Character("포세이돈", "마법사", 20);
 		Character c = new Character("아프로디테", "마법사", 1);
-		Character d = new Character("아프로디테", "궁수");
 		
 		a.show();
 		b.show();
 		c.show();
-		d.show();
 
 	}
 

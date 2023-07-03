842d6b65c2399cd0e3d838bb70d656fc75e58fd0
newstar95 <byeol12120@gmail.com>
2023-07-03 16:49:55 +0900

    캐릭터 정보 문제

---
+++ day11/src/oop/constructor3/Test01.java
@@ -0,0 +1,18 @@
+package oop.constructor3;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		Character a = new Character("헤라클레스", "전사", 50);
+		Character b = new Character("포세이돈", "마법사", 20);
+		Character c = new Character("아프로디테", "마법사", 1);
+		Character d = new Character("아프로디테", "궁수");
+		
+		a.show();
+		b.show();
+		c.show();
+		d.show();
+
+	}
+
+}

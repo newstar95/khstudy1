aac7d16afc49658d24067bc763539e6430d9328e
newstar95 <byeol12120@gmail.com>
2023-07-04 14:00:26 +0900

    참조형 데이터(ex : 배열) 메소드 처리

--- day12/src/oop/keyword4/Robot.java
+++ day12/src/oop/keyword4/Robot.java
@@ -1,5 +1,7 @@
 package oop.keyword4;
 
+import java.util.Random;
+
 public class Robot {
 	//[1] 제곱 계산 기능
 	public static int square(int num) {
@@ -33,27 +35,74 @@ public class Robot {
 	public static float circle(float radius) {
 		return square(radius);
 	}
-}
-
-//	public static int Sort(int a[]) {
-//
-//			for(int i = 0; i < a.length + 1; i++) {
-//				int minIndex = i;
-//				for(int k = i + 1; k < a.length; k++ ) {
-//					if(a[minIndex] > a[k]) {
-//						minIndex = k;
-//					}
-//					
-//					int backup = a[minIndex];
-//					a[minIndex] = a[k];
-//					a[k] = backup;
-//					
-//				}
-//				
-//			}
-//			for ( int i = 0; i < a.length; i++) {
-//				return a[i];
-//			}
-//	
-//		}
+	
+	//[4] 배열을 선택정렬하는 메소드
+	//공개, 객체 없이, 반환 없이
+	public static void sort(int[] data) {
+		//선택 정렬
+		for(int k=0; k < data.length-1; k++) {
+			int minIndex = k;//k번 위치의 값이 가장 작다고 생각
+			for(int i=k+1; i < data.length; i++) {//뒤에 있는 데이터와 비교하여
+				if(data[minIndex] > data[i]) {//더 작은 값이 있다면
+					minIndex = i;//교체!
+				}
+			}
+			//System.out.println("minIndex = " + minIndex);
+			
+			int backup = data[minIndex];
+			data[minIndex] = data[k];
+			data[k] = backup;
+		}
+	}
+	
+	//[5] 배열을 뒤집는 메소드(reverse)
+	public static void reverse(int[] data) {
+		int left = 0; //처음위치
+		int right = data.length-1; //마지막위치
+	
+		for (int i = 0; i < data.length/2; i++) {
+			int backup = data[left];
+			data[left] = data[right];
+			data[right] = backup;
+			
+			left++;
+			right--;
+			}
+		
+		}
+	
+	//[6]배열을 무작위로 섞는 메소드(shuffle)
+	public static void shuffle(int[] data) {
+		Random r = new Random();
+		
+		for(int i = 0; i < data.length; i++) {
+			//셔플(뒤섞기)
+			//- 같은 자리 제거
+			//- 같은 자리가 나오면 다시 뽑아라 / 같은 자리가 나오면 하던 작업을(지금 하는 턴을) 무효화시켜라
+			//[i] <----> [0~4]
+			int index = r.nextInt(data.length);
+		
+				if(i == index) {
+					i--;
+					//continue 를 쓰거나 else문을 쓴다 
+				} else {
+					int backup = data[i];
+					data[i] = data[index];
+					data[index] = backup;
+				}
+				
+			}
+		
+		}
+	
+	//[7]배열을 출력하는 메소드
+	public static void print(int data[]) {
+		for ( int i = 0; i < data.length; i++) {
+			System.out.print( data[i] );
+			System.out.print("\t");
+		}
+		System.out.println();
+	}
+	
+	}	
 
--- day12/src/oop/keyword4/Test01.java
+++ day12/src/oop/keyword4/Test01.java
@@ -12,6 +12,17 @@ public class Test01 {
 		double result3 = Robot.circle(5);
 		System.out.println("원의 넓이: " + result3);
 		
+		//(주의) 참조형 데이터는 리모컨만 전달하면 외부에서 직접 제어 가능
+		int[] data = new int[] {30, 50, 20, 10, 40};
+		
+		Robot.sort(data); //리모컨 전달(call-by-reference)
+		Robot.print(data);
+		
+		Robot.reverse(data);
+		Robot.print(data);
+		
+		Robot.shuffle(data);
+		Robot.print(data);
 	}
 
 }

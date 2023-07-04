59baa92aa56ec762114ab5e782d3d16469264ec5
newstar95 <byeol12120@gmail.com>
2023-07-04 11:08:03 +0900

    객체지향의 문제점

--- day12/src/oop/keyword1/Test01.java
+++ day12/src/oop/keyword1/Test01.java
@@ -3,7 +3,10 @@ package oop.keyword1;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 10 + 20을 객체지향 방식으로처리
+		Calculator a = new Calculator(10, 20);
+		int result = a.getTotal();
+		System.out.println("result = " + result);
 
 	}
 

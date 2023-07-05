1a068d57fdcc4274b37c9529df82dcef86f5183d
newstar95 <byeol12120@gmail.com>
2023-07-05 17:43:00 +0900

    캐릭터 생성 문제

--- day13/src/oop/inherit8/Archer.java
+++ day13/src/oop/inherit8/Archer.java
@@ -1,25 +1,19 @@
 package oop.inherit8;
 
-public class Archer extends Function {
+public class Archer extends Player {
 
-	public Archer(String name, int level) {
-		super(name, level);
+	public Archer(String id) {
+		super(id);
 	}
-	
-	public Archer(String name) {
-		super(name, 1);
-	}
-	
+
 	@Override
 	public void attack() {
 		System.out.println("궁수가 활로 공격합니다!");
 	}
-
 	@Override
 	public void move() {
 		System.out.println("궁수가 숨어서 이동합니다!");
 	}
-
 	@Override
 	public void store() {
 		System.out.println("궁수가 상점을 개설합니다!");
--- day13/src/oop/inherit8/Warrior.java
+++ day13/src/oop/inherit8/Warrior.java
@@ -1,25 +1,19 @@
 package oop.inherit8;
 
-public class Warrior extends Function {
+public class Warrior extends Player{
 
-	public Warrior(String name,int level) {
-		super(name, level);
+	public Warrior(String id) {
+		super(id);
 	}
 
-	public Warrior(String name) {
-		super(name, 1);
-	}
-	
 	@Override
 	public void attack() {
 		System.out.println("전사가 칼로 공격합니다!");
 	}
-
 	@Override
 	public void move() {
 		System.out.println("전사가 뛰어서 이동합니다!");
 	}
-
 	@Override
 	public void store() {
 		System.out.println("전사가 상점을 개설합니다!");

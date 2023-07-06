e110d51b05dc5ae7608aa0b4efdb11a9f2eac4a3
newstar95 <byeol12120@gmail.com>
2023-07-06 13:01:34 +0900

    인터페이스와 다중상속

---
+++ day14/src/oop/multi1/Person.java
@@ -0,0 +1,23 @@
+package oop.multi1;
+
+//핵심 상속(클래스 상속)은 extends 키워드로 구현
+//부가 상속(인터페이스 상속)은 implements 키워드로 구현
+public class Person implements Singer, Programmer {
+
+	@Override
+	public void conding() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sing() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+}

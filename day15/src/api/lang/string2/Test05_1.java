3a3deb41402c33100ed3523452f2e7cbeac69068
newstar95 <byeol12120@gmail.com>
2023-07-07 18:16:15 +0900

    올바른 날짜 판정

--- day15/src/api/lang/string2/Test05_1.java
+++ day15/src/api/lang/string2/Test05_1.java
@@ -1,29 +1,18 @@
 package api.lang.string2;
 
-import java.util.Scanner;
-
 public class Test05_1 {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("생년월일 입력(yyyy-mm-dd)");
-		String user = sc.nextLine();
+		String birth = "1999-04-31";
 		
-		sc.close();
+		String regex = "^(19[0-9]{2}|20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01])$";
 		
-		String regex=
-				"^([1][9][0-9][0-9]|[2][0-9][0-9][0-9])\\-(0[1-9]|[1][012])\\-([0][1-9]|[12][1-9]|[3][01])$";
-				
-		
-		boolean isValid = user.matches(regex);
-		
-		if(isValid) {
-			System.out.println("올바른 생년월일입니다.");
-		} else {
-			System.out.println("잘못된 생년월일입니다.");
+		if(birth.matches(regex)) {
+			System.out.println("올바른 생년월일");
+		}
+		else {
+			System.out.println("잘못된 생년월일");
 		}
 		
 	}
-
 }

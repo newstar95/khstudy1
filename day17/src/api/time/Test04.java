21565ce96e0560ecb46b59d6d60ca31c8578b096
newstar95 <byeol12120@gmail.com>
2023-07-11 15:31:21 +0900

    Calendar 예제

--- day17/src/api/time/Test04.java
+++ day17/src/api/time/Test04.java
@@ -3,42 +3,42 @@ package api.time;
 import java.util.Calendar;
 
 public class Test04 {
-
 	public static void main(String[] args) {
 		//Calendar의 장점
 		//= 1900년이 기준이 아니기 때문에 일반적인 연도 설정 가능
-		//= 범위를 벗어나도 자동으로 가능한 값으로 변환된다.(계산 특화)
+		//= 범위를 벗어나도 자동으로 가능한 값으로 변환된다(계산 특화)
 		
 		Calendar c = Calendar.getInstance();
 		
 		//항목별로 설정
-		c.set(Calendar.YEAR, 2000);
-		c.set(Calendar.MONTH, 0); //1월이 0부터 시작(주의)
-		c.set(Calendar.DATE, 1);
-		c.set(2000, 0, 10);
+		//c.set(Calendar.YEAR, 2000);
+		//c.set(Calendar.MONTH, 0);//1월이 0부터 시작(주의)
+		//c.set(Calendar.DATE, 1);
+		//c.set(2000, 0, 1);
+		//c.set(2000, 0, 100);
+		//c.set(2000, 0, 0);
+		c.set(2000, 30, 1);
 		
 		//항목별로 추출하여 출력
 		int year = c.get(Calendar.YEAR);
-		System.out.println(year);
+		System.out.println("연 = " + year);
 		
 		//(주의) 월은 0부터 11까지 관리됨
+		int month = c.get(Calendar.MONTH)+1;
+		System.out.println("월 = " + month);
 		
-		int month = c.get(Calendar.MONTH) + 1;
-		System.out.println(month);
-		
-//		int day = Calendar.DATE;
-		int day = Calendar.DAY_OF_MONTH;
+//		int day = c.get(Calendar.DATE);
+		int day = c.get(Calendar.DAY_OF_MONTH);
 		System.out.println("일 = " + day);
 		
-//		int hour = Calendar.HOUR; //12시간 방식
-		int hour = Calendar.HOUR_OF_DAY; //24시간 방식
-		System.out.println("시간 = " + hour);
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		System.out.println("시 = " + hour);
 		
-		int minute = Calendar.MINUTE;
+		int minute = c.get(Calendar.MINUTE);
 		System.out.println("분 = " + minute);
 		
-		int second = Calendar.SECOND;
+		int second = c.get(Calendar.SECOND);
 		System.out.println("초 = " + second);
 	}
-
 }
+

d53ccecb983b2934bf823cea0a092b66f8826756
newstar95 <byeol12120@gmail.com>
2023-07-12 16:00:31 +0900

    끝말잇기 게임

---
+++ day18/src/api/collection/Test06.java
@@ -0,0 +1,46 @@
+package api.collection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<String> chain = new ArrayList<>();
+		chain.add("자바");
+		
+		String regex = "^[가-힣]{2,6}$";
+		
+		while (true) {
+			String given = chain.get(chain.size()-1); //마지막 기록을 추출
+			
+			System.out.println("제시어: " + given);
+			System.out.print("입력: ");
+			String input = sc.next();
+
+			if(input.matches(regex) == false) {
+				break;
+			} else if (given.charAt(given.length()-1) != input.charAt(0)) {
+				break;
+			} else if(chain.contains(input)) {
+				break;
+			}
+			else {
+				System.out.println("통과");
+				chain.add(input);
+			}
+		}
+			System.out.println("게임 오버!");
+			
+			System.out.println("기록");
+			for(int i = 0; i < chain.size(); i++) {
+				String word = chain.get(i);
+
+				System.out.println("-> " + word);
+			}
+			
+	 }
+
+}

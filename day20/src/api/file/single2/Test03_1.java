bf80677d6e9194a1776443e147e8c70e8b3b09a0
newstar95 <byeol12120@gmail.com>
2023-07-14 18:14:21 +0900

    과제풀이

--- day20/src/api/file/single2/Test03_1.java
+++ day20/src/api/file/single2/Test03_1.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
-public class Test03 {
+public class Test03_1 {
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -16,12 +16,20 @@ public class Test03 {
 	File writeTarget = new File("./sample/copy2.txt");
 	FileOutputStream writeStream= new FileOutputStream(writeTarget);
 	
-	byte[] buffer = new byte[127];
+	byte[] buffer = new byte[100];
+	
+	long count = 0L;
+	long total = readTarget.length();
+	DecimalFormat fmt = new DecimalFormat("#,##0.00");
 	
 	while(true) {
 		int a = readStream.read();
 		if(a == -1) break;
 			writeStream.write(a);
+			count++;
+			
+			double percent = count * 100d / total;
+			System.out.println(count + "/" + total + "(" + fmt.format(percent) + "%)");
 		}
 
 	//정리

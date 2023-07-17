bf4553db7f8e068932069611ae5220c53b530a81
newstar95 <byeol12120@gmail.com>
2023-07-17 18:19:42 +0900

    백준

---
+++ baekjoon/src/Test/Main.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String site = "joonas";
+		String input = sc.next();
+		System.out.println(input + "??!");
+		
+		}
+}
--- baekjoon/src/baekjoon/Test7.java
+++
@@ -1,9 +0,0 @@
-package baekjoon;
-
-public class Test7 {
-
-	public static void main(String[] args) {
-
-	}
-
-}

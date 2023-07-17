e732758581e39635541f443d4d1d573b75d5a477
newstar95 <byeol12120@gmail.com>
2023-07-17 18:12:02 +0900

    게임 문제 풀이 업로드

---
+++ day21/src/api/file/object/Test07.java
@@ -0,0 +1,9 @@
+package api.file.object;
+
+import java.io.IOException;
+
+public class Test07 {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		GameUtil.play();
+	}
+}

7e2379f6ea9f01bf6720c155d0b3832409f77ea5
newstar95 <byeol12120@gmail.com>
2023-07-24 16:54:28 +0900

    포켓몬 등록 최종 형태

--- jdbc03/src/jdbc/dao/PocketmonDao.java
+++ jdbc03/src/jdbc/dao/PocketmonDao.java
@@ -2,6 +2,7 @@ package jdbc.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import jdbc.dto.PocketmonDto;
 import jdbc.util.JdbcUtils;
 
 //DAO 클래스
@@ -18,6 +19,17 @@ public class PocketmonDao {
 		Object[] data = {no, name, type};
 		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
 		jdbcTemplate.update(sql, data);
+		
 	}
+
 	
+	//바람직한 형태의 등록 메소드
+	public void insert(PocketmonDto dto) {
+		String sql = "insert into pocketmon(no, name, type) "
+				+ "values(?, ?, ?)";
+		
+		Object[] data = {dto.getNo(), dto.getName(), dto.getType()};
+		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+		jdbcTemplate.update(sql, data);
+	}
 }

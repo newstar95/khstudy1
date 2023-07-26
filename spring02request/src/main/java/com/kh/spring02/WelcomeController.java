a5a0e564d61419ab477bc0f2b76a9e55b797aa16
newstar95 <byeol12120@gmail.com>
2023-07-26 14:08:39 +0900

    컨트롤러 샘플

---
+++ spring02request/src/main/java/com/kh/spring02/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.kh.spring02;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Spring Framework!";
+	}
+}

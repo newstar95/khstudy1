7081dc158cd5e8d1e85657a0148f227e04cf4ac1
newstar95 <byeol12120@gmail.com>
2023-09-08 10:33:10 +0900

    spring project11 생성

---
+++ springhome11ajax/src/main/java/com/kh/spring11/Springhome11ajaxApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springhome11ajaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springhome11ajaxApplication.class, args);
+	}
+
+}

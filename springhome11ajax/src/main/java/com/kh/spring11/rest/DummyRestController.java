d2c9471f18cc737970348cd80cd5964bb4a15d13
newstar95 <byeol12120@gmail.com>
2023-09-11 20:57:01 +0900

    백엔드 구현

--- springhome11ajax/src/main/java/com/kh/spring11/rest/DummyRestController.java
+++ springhome11ajax/src/main/java/com/kh/spring11/rest/DummyRestController.java
@@ -1,5 +1,6 @@
 package com.kh.spring11.rest;
 
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,22 +13,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring11.dao.MemberDao;
+import com.kh.spring11.dao.PocketmonDao;
 import com.kh.spring11.dto.MemberDto;
+import com.kh.spring11.dto.PocketmonDto;
 
 //CORS를 해제하기 위한 설정(Annotation)
-//@CrossOrigin //전부다 허용(위험!)
-@CrossOrigin(origins = {"http://127.0.0.1:5501"}) //vscode 홈페이지 주소
-@RestController //@Controller + @responseBody 
+@CrossOrigin//전부다 허용(위험!)
+//@CrossOrigin(origins = {"http://192.168.30.200:5500"})
+@RestController//@Controller + @ResponseBody
 public class DummyRestController {
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello spring ajax";
 	}
 	
-	//Rest Controller에서는 내가 전해줄 데이터가 반환형이 된다.
+	//Rest Controller에서는 내가 전해줄 데이터가 반환형이 된다
 	//- 자동으로 Spring에서 JSON형태로 변환하여 반환
-	//- 변환을 담당하는 라이브러리 jackson-databind
+	//- 변환을 담당하는 라이브러리는 jackson-databind
 	@RequestMapping("/lotto")
 	public Set<Integer> lotto() {
 		Random r = new Random();
@@ -45,10 +48,10 @@ public class DummyRestController {
 	@PostMapping("/idCheck")
 	public String idCheck(@RequestParam String memberId) {
 		MemberDto memberDto = memberDao.selectOne(memberId);
-		if(memberDto != null) { //아이디가 있으면
+		if(memberDto != null) {//아이디가 있으면
 			return "Y";
 		}
-		else { //아이디가 없으면
+		else {//아이디가 없으면
 			return "N";
 		}
 	}
@@ -56,7 +59,7 @@ public class DummyRestController {
 	@PostMapping("/nicknameCheck")
 	public String nicknameCheck(@RequestParam String memberNickname) {
 		MemberDto memberDto = 
-				memberDao.selectOneByMemberNickname(memberNickname);
+			memberDao.selectOneByMemberNickname(memberNickname);
 		if(memberDto == null) {
 			return "Y";
 		}
@@ -64,4 +67,14 @@ public class DummyRestController {
 			return "N";
 		}
 	}
+	
+	@Autowired
+	private PocketmonDao pocketmonDao;
+	
+	//프론트엔드에 포켓몬 목록을 반환하는 매핑
+	@RequestMapping("/pocketmon")
+	public List<PocketmonDto> pocketmon() {
+		return pocketmonDao.selectList();
+	}
+	
 }

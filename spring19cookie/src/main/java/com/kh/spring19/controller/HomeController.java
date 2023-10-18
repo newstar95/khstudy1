440a6ffa831ed9d68ab9d7847e3e744622f011e7
newstar95 <byeol12120@gmail.com>
2023-10-18 10:26:33 +0900

    쿠키로 아이디 저장하기

---
+++ spring19cookie/src/main/java/com/kh/spring19/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.kh.spring19.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home"; //기존
+	}
+}
